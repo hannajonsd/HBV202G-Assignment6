d91854f1c9f02cae5442f79fcdc939b5afa8b758
hannajonsd <hanna.m.jonsd@gmail.com>
2023-02-21 10:50:03 +0000

    refactor done

--- src/test/java/is/hi/hbv202g/ass6/CarOwnerTest.java
+++ src/test/java/is/hi/hbv202g/ass6/CarOwnerTest.java
@@ -1,16 +1,33 @@
 package is.hi.hbv202g.ass6;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CarOwnerTest {
 
+    public static final String JOHN_DOE = "John Doe";
+    public static final String BEETLE = "Beetle";
+
+    CarOwner carOwner;
+
+    Car car;
+
+    @Before
+    public void setUpOwner() throws Exception {
+        carOwner = new CarOwner(JOHN_DOE, car);
+    }
+
+    @Before
+    public void setUpCar() throws Exception {
+        car = new Car(BEETLE);
+    }
+
+
     @Test
     public void getName() {
-        Car car=new Car("Beetle");
-        CarOwner carOwner = new CarOwner("John Doe", car);
-        assertEquals("John Doe", carOwner.getName());
+        assertEquals(JOHN_DOE, carOwner.getName());
     }
 
 }
